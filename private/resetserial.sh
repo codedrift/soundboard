0bbcbb9a56639406044e7fcf7c55af65168c07a6
Peter Mösenthin <peter.moesenthin@gmail.com>
2015-10-22 20:11:44 +0200

    change username var

--- private/resetserial.sh
+++ private/resetserial.sh
@@ -1,7 +1,9 @@
+METEORUSER=user1234
+
 sudo chmod o+rw /dev/ttyUSB0
 sudo echo ^v^o > /dev/ttyUSB0
-sudo usermod -a -G dialout chaosboard
+sudo usermod -a -G dialout $METEORUSER
 sudo chmod 775 /dev/ttyUSB0
 screen -dm /dev/ttyUSB0 19200
 pgrep screen | xargs kill -9
-sudo chown chaosboard:dialout /dev/ttyUSB0
+sudo chown $METEORUSER:dialout /dev/ttyUSB0
