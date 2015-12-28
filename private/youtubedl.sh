2d67ad33fb549415258ba07eb16e92a0d599da19
Peter Mösenthin <moesenthin@sipgate.de>
2015-12-28 11:48:59 +0100

    fix youtube

--- private/youtubedl.sh
+++ private/youtubedl.sh
@@ -10,19 +10,29 @@ mkdir -p $1/youtube
 cd $1/youtube
 
 names=$(youtube-dl --get-filename --audio-format mp3 -e -x $2)
+
 duration=$(youtube-dl --get-duration -e -x $2)
+
 title=$(echo "$names" | head -1 | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+
 file=$(echo "$names" | tail -1)
+
 title=$(echo "$title" | sed 's/Ä/Ae/g' | sed 's/Ö/Oe/g' | sed 's/Ü/Ue/g' | sed 's/ä/ae/g' | sed 's/ö/oe/g' | sed 's/ü/ue/g' | sed 's/ß/ss/g' | sed 's/[^a-z0-9_-]//g')
-mp3file=$(echo "$file" | cut -d '.' -f1).mp3
+
+mp3file=$(basename "$file" "${file##*.}")"mp3"
+
+#mp3file="$mp3file"mp3
 
 youtube-dl --audio-format mp3 -x "$2"
 
-echo "start " $3
-echo "end " $4
-
 title="$title"_"$3"-"$4"
 
-cutmp3 -i "$mp3file" -O "$title".mp3 -a $3 -b $4
+echo "MP3 file: " $mp3file
+echo "File: " $file
+echo "Escaped title: " $title
+echo "Start time: " $3
+echo "End time: " $4
 
-rm "$mp3file"
+cutmp3 -i "$mp3file" -O "$title".mp3 -a "$3" -b "$4"
+
+#rm "$mp3file"
--- server/system.js
+++ server/system.js
@@ -44,9 +44,8 @@ grabYT = function grabYT (yturl,ytfrom,ytto){
 	var params = ['assets/app/youtubedl.sh', getSoundFilesDir(), yturl, ytfrom, ytto];
 	var command = Spawn('sh', params);
 	console.log("grabbing youtube sounds with params " + params);
-	if(wrapSpawnCommand(command, 'grabYT')){
-		updateFsSoundsCollections();
-	}
+	wrapSpawnCommand(command, 'grabYT');	
+	updateFsSoundsCollections();
 };
 
 wrapSpawnCommand = function wrapSpawnCommand(command, name) {
